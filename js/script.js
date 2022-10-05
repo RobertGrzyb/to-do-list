1c217bcfb683f8cade51f15040f26865323f8a8d
Robert Grzyb <robertgrzyb96@gmail.com>
2022-10-05 23:41:50 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -11,6 +11,19 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+    
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+                render();
+    }
+
     const render = () => {
         let htmlString = "";
 
@@ -19,21 +32,24 @@
                     <li
                     ${task.done ? " style=\"text-decoration: line-through\"" : ""}
                     >
+                    <button class="js-remove">usuń</button>
                     ${task.content}
                     </li>
                 `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
     };
     
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-
-        render();
-    };
+    
     
     const onFormSubmit = (event) => {
         event.preventDefault();
